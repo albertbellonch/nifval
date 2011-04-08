25957c5f9b647a9fd7bcf30ba86b900797b44f4a
Roger Campos <roger@itnig.net>
2011-04-08 17:52:33 +0200

    adding .to_i to make it work with 1.9.2

--- lib/nifval.rb
+++ lib/nifval.rb
@@ -25,15 +25,15 @@ module NifVal
         nif[8] == "TRWAGMYFPDXBNJZSQVHLCKE"[nif[0..7].to_i % 23]
       else
         # CIF algorithm
-        sum = nif[2]-48 + nif[4]-48 + nif[6]-48
+        sum = nif[2].to_i-48 + nif[4].to_i-48 + nif[6].to_i-48
         [1,3,5,7].each do |i|
-          t = (2*(nif[i]-48)).to_s
-          t1 = t[0]-48
-          t2 = t[1].nil? ? 0 : t[1]-48
+          t = (2*(nif[i].to_i-48)).to_s
+          t1 = t[0].to_i-48
+          t2 = t[1].nil? ? 0 : t[1].to_i-48
           sum += t1+t2
         end
         sumstr = sum.to_s
-        n = 10 - (sumstr[sumstr.length-1]-48)
+        n = 10 - (sumstr[sumstr.length-1].to_i-48)
 
         if nif.match(/^[KLM]{1}/)
           # Special NIFs (as CIFs)
